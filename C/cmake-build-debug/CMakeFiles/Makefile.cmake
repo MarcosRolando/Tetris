940a1da1d57916594dac8ec234963853d3ce37f8
Marcos Rolando <marcosrolando.mr@gmail.com>
2021-03-27 12:33:07 -0300

    Cambio el nombre de foo a tetris

--- C/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ C/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -49,5 +49,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/foo.dir/DependInfo.cmake"
+  "CMakeFiles/viewer.dir/DependInfo.cmake"
   )
--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("cargo:rustc-link-search=C/cmake-build-debug");
-    println!("cargo:rustc-link-lib=foo");
+    println!("cargo:rustc-link-lib=viewer");
     println!("cargo:rustc-link-lib=SDL2");
     println!("cargo:rustc-link-lib=SDL2_image");
     println!("cargo:rustc-link-lib=SDL2_mixer");
