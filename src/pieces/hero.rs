ee8d910b8f914ef6a7199dbaa0f1c96ed7e66f4a
Marcos Rolando <marcosrolando.mr@gmail.com>
2021-03-29 11:20:48 -0300

    Termino de cambiar los ejes de las piezas, ahora chequeo las colisiones con las mejores coordenadas posibles

--- src/pieces/hero.rs
+++ src/pieces/hero.rs
@@ -2,11 +2,11 @@ use crate::pieces::piece::{PieceType, Position, Orientation, Rotation, TakenTile
 use crate::game::{Board, TileState};
 
 /* This is the I piece */
-/* This piece position is fixed on the left corner on the Default Orientation */
+/* This piece position is fixed on the center right (Default Orientation) */
 /*
 Default Orientation
 
-x * * *
+* * x *
 
  */
 
@@ -35,12 +35,12 @@ impl PieceType for Hero {
 
     /* Returns an array of 4 elements of Positions if it collided, otherwise returns None */
     fn check_default_collision(&self, board: &Board, position: &Position) -> Option<TakenTiles> {
-        for i in position.column..(position.column + 4) {
-            if board[position.row - 1][i] == TileState::Taken {
-                return Some([*position,
-                             Position{column:position.column+1,..*position},
-                             Position{column:position.column+2,..*position},
-                             Position{column:position.column+3,..*position},
+        for i in (position.column - 2)..(position.column + 2) {
+            if board[position.row][i] == TileState::Taken {
+                return Some([Position{row:position.row+1,..*position},
+                             Position{column:position.column+1,row:position.row+1},
+                             Position{column:position.column-1,row:position.row+1},
+                             Position{column:position.column-2,row:position.row+1},
                ]);
             }
         }
@@ -49,16 +49,16 @@ impl PieceType for Hero {
 
     /* Returns an array of 4 elements of Positions if it collided, otherwise returns None */
     fn check_inverted_collision(&self, board: &Board, position: &Position) -> Option<TakenTiles> {
-        self.check_default_collision(board, &Position{column:position.column-3,..*position})
+        self.check_default_collision(board, position)
     }
 
     /* Returns an array of 4 elements of Positions if it collided, otherwise returns None */
     fn check_right_collision(&self, board: &Board, position: &Position) -> Option<TakenTiles> {
-        if board[position.row - 4][position.column] == TileState::Taken {
+        if board[position.row - 1][position.column] == TileState::Taken {
             Some([*position,
-                Position{row:position.row-1,..*position},
-                Position{row:position.row-2,..*position},
-                Position{row:position.row-3,..*position},
+                Position{row:position.row+1,..*position},
+                Position{row:position.row+2,..*position},
+                Position{row:position.row+3,..*position},
             ])
         } else {
             None
@@ -66,8 +66,9 @@ impl PieceType for Hero {
     }
 
     /* Returns an array of 4 elements of Positions if it collided, otherwise returns None */
+    /* In Classic Tetris this rotation is actually the same as the right rotation */
     fn check_left_collision(&self, board: &Board, position: &Position) -> Option<TakenTiles> {
-        self.check_right_collision(board, &Position{row:position.row+3,..*position})
+        self.check_right_collision(board, position)
     }
 
     /* Changes the piece position that corresponds to the given Orientation */
